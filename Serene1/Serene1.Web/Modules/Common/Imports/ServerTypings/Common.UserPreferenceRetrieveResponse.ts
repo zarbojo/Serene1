22af78a0de8649b2c16557d3a4bc800026005788
zbojovic <zbojovic@BG-BOJOVIC1>
2016-06-16 15:29:06 +0200

    Add project files.

---
+++ Serene1/Serene1.Web/Modules/Common/Imports/ServerTypings/Common.UserPreferenceRetrieveResponse.ts
@@ -0,0 +1,6 @@
+﻿namespace Serene1.Common {
+    export interface UserPreferenceRetrieveResponse extends Serenity.ServiceResponse {
+        Value?: string
+    }
+}
+
---
+++ Serene1/Serene1.Web/Modules/Common/Imports/ServerTypings/Common.UserPreferenceUpdateRequest.ts
@@ -0,0 +1,8 @@
+﻿namespace Serene1.Common {
+    export interface UserPreferenceUpdateRequest extends Serenity.ServiceRequest {
+        PreferenceType?: string
+        Name?: string
+        Value?: string
+    }
+}
+
